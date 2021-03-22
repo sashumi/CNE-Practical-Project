394470f4532dfaedf1331be4d38107bbd81e1498
sashumi <55672042+sashumi@users.noreply.github.com>
2021-03-22 23:57:04 +0000

    try adding vm for jenkins installation

---
+++ jenkins/4-jenkins-vm.tf
@@ -0,0 +1,32 @@
+resource "aws_key_pair" "shamsi_jenkins" {
+  key_name = "shamsi_jenkins"
+  public_key = var.ssh_pubkey
+}
+
+resource "aws_eip" "jenkins_ip" {
+  // not adding any parameter. checking if it works
+}
+
+resource "aws_instance" "jenkins" {
+  ami = var.ami
+  availability_zone = aws_subnet.subnet1.availability_zone
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.shamsi_jenkins
+  vpc_security_group_ids = [
+    aws_security_group.jenkins_sg_22.id,
+    aws_security_group.jenkins_sg_80.id]
+  subnet_id = aws_subnet.subnet1.id
+
+  tags = {
+    Name = "jenkinsvm"
+  }
+
+  root_block_device {
+    volume_size = 100
+  }
+}
+
+resource "aws_eip_association" "jenkins_ip" {
+  instance_id = aws_instance.jenkins.id
+  allocation_id = aws_eip.jenkins_ip.id
+}

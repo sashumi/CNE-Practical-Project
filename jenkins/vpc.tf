ae1baa7b5bcb56f9faf3328fae5d697b3e00ab55
sashumi <55672042+sashumi@users.noreply.github.com>
2021-03-22 23:30:45 +0000

    add subnets to vpc

--- jenkins/vpc.tf
+++ jenkins/vpc.tf
@@ -2,12 +2,42 @@ resource "aws_vpc" "jenkins" {
 
   cidr_block = "10.10.0.0/16"
   instance_tenancy = "default"
-  enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_hostnames = true # not sure what this is, but using from example
+  enable_dns_support = true # not sure what this is, but using from example
 
   tags = merge(
   {
     "Name" = "jenkins"
   }
   )
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.jenkins.id
+  availability_zone = "eu-west-1a"
+  cidr_block = "10.10.10.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.jenkins.id
+  availability_zone = "eu-west-1b"
+  cidr_block = "10.10.20.0/24"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id = aws_vpc.jenkins.id
+  availability_zone = "eu-west-1c"
+  cidr_block = "10.10.30.0/24"
+
+  tags = {
+    Name = "subnet3"
+  }
 }
